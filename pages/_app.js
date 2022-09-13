6cd72beb38a680f6a69b4b667933a5eaf146d11c
kevinrp22 <kevinpuchaicela@gmail.com>
2022-09-13 15:30:37 +0200

    MUI configured

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider } from "@mui/material";
+import theme from "../styles/theme";
+import clientSideEmotionCache from "../styles/createEmotionCache";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
